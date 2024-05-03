1b2db6c6f75d800c90993dd9862815613bfd6a33
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-05-03 15:20:41 +0530

    MostFrequentElement.java

---
+++ MostFrequentElement.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+	public static void main(String[] args) {
+	
+		int arr[]= {1,2,2,1,1,3,4,5,1,2,1,1};
+		int max=0;
+		int element=0;
+		for(int i=0;i<arr.length;i++) {
+			int count=0;
+			for(int j=0;j<arr.length;j++) {
+				if(arr[i]==arr[j]) {
+					count++;
+				}
+			}
+			if(count>max) {                                                                      //OUTPUT : element:1 max:6
+				max=count;
+				element=arr[i];
+			}
+		}
+		System.out.print("element"+":" +element +" max"+":" +max);
+
+	}
+}
